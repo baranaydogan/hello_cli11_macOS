76f5ea965a03851c98e42ad34034b98dc4552020
Baran Aydogan <baran.aydogan@gmail.com>
2025-02-06 21:39:48 +0200

    .

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "CLI11.hpp"
+#include <string>
+
+
+int main(int argc, char** argv) {
+    CLI::App app{"App description"};
+    argv = app.ensure_utf8(argv);
+
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "A help string");
+
+    CLI11_PARSE(app, argc, argv);
+    return 0;
+}
---
+++ test_macOS.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Requires LLVM and libomp
+llvm_prefix=$(brew --prefix llvm@18)
+
+if ! brew list | grep -q "llvm@18"; then
+  brew install llvm@18
+fi
+
+# Set environment variables for this script
+export PATH="${llvm_prefix}/bin:$PATH"
+export LDFLAGS="-L${llvm_prefix}/lib"
+
+${llvm_prefix}/bin/clang++ main.hpp -o helloCLI11
+
+chmod +x helloCLI11
+
+echo "1"
+./helloCLI11
+
+echo "2"
+./helloCLI11 --help
+
+echo "3"
+./helloCLI11 --file
+
+echo "4"
+./helloCLI11 --badoption
+
+echo "Done"
+
